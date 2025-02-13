228eaff7fd1e8ddbc8d328748bbc0f8d962d2ca1
psyev2 <psyev2@exmail.nottingham.ac.uk>
2025-02-13 17:52:57 +0000

    Updated variable names and docstring for less ambiguity

--- F1TenthAlgorithms/desired_heading.py
+++ F1TenthAlgorithms/desired_heading.py
@@ -1,22 +1,27 @@
 from math import atan2
 
-def find_desired_heading(x: float, y:float, x_des: float, y_des: float) -> float:
+def find_desired_heading(current_x_position_m: float, current_y_position_m: float, desired_x_position_m: float, desired_y_position_m: float) -> float:
     """
     Calculate the desired heading angle based on the current and target positions.
+    This function works within any frame, as long as inputs are all within the same frame.
 
     Parameters:
-        x:
-            Current x-coordinate.
-        y:
-            Current y-coordinate.
-        x_des:
-            Target x-coordinate.
-        y_des:
-            Target y-coordinate.
+        current_x_position_m:
+            Current x-coordinate,
+            measured in metres.
+        current_y_position_m:
+            Current y-coordinate,
+            measured in metres.
+        desired_x_position_m:
+            Desired x-coordinate,
+            measured in metres.
+        desired_y_position_m:
+            Desired y-coordinate,
+            measured in metres.
 
     Returns:
         desired_heading:
             The desired heading angle in radians, measured counterclockwise
-            from the positive x-axis.
+            from the positive x-axis. This will be in the same frame as the inputs.
     """
-    return atan2(x_des - x, y_des - y)
+    return atan2(desired_x_position_m - current_x_position_m, desired_y_position_m - current_y_position_m)
