65b55684d92c0aca1f1382d1f95258765c4d90ef
Rsleiman <ralphsleiman16@gmail.com>
2025-02-14 00:20:47 +0000

    Implemented suggestions from Jason, followed Ric's doxygen comment standards: - Added "control" to function name - Split def line across multiple lines to improve readability - Changed parameter names and added units at the end of each (if applicable) - Improved doxygen descriptions and readability (matched to Ric's format)

--- F1TenthAlgorithms/proportional_steering_angle.py
+++ F1TenthAlgorithms/proportional_steering_angle.py
@@ -1,13 +1,26 @@
-def proportional_steering_angle(*, k:int=1, heading_des:float, heading_curr:float) -> float:
+def proportional_steering_angle_control(
+        *,
+        k:int=1,
+        desired_heading_rad:float,
+        current_heading_rad:float
+    ) -> float:
     """
-    Calculate the steering angle based on the difference between the desired heading and the current heading.
+    Calculate the steering angle based on the difference between the desired heading and the current heading, scaled by a proportional gain.
 
     Parameters:
-    k (float): The proportional gain.
-    heading_des (float): The desired heading in radians.
-    heading_curr (float): The current heading in radians.
+        k:
+            The proportional gain used to scale the heading error,
+            this is a constant.
+        desired_heading_rad:
+            The desired heading the car should be facing,
+            measured in radians.
+        current_heading_rad:
+            The current heading the car is facing,
+            measured in radians.
 
     Returns:
-    float: The steering angle in radians.
+        steering_angle_rad:
+            The steering angle the car should use to correct its heading,
+            measured in radians.
     """
-    return k*(heading_des - heading_curr)
+    return k*(desired_heading_rad - current_heading_rad)
