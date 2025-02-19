2f360adae2881657c5f1973583d6e220f05b322a
Rsleiman <ralphsleiman16@gmail.com>
2025-02-19 16:09:43 +0000

    Implemented recommended adjustments from PR: - Wrapped doxygen description to align with PEP8 format guidelines. - Changed variable names to be consistent with all other members' - changed function name

--- F1TenthAlgorithms/proportional_steering_angle.py
+++ F1TenthAlgorithms/proportional_steering_angle.py
@@ -1,26 +1,27 @@
-def proportional_steering_angle_control(
+def find_proportional_steering_angle_control(
         *,
         k:int=1,
-        desired_heading_rad:float,
-        current_heading_rad:float
+        desiredHeading_fix_rad:float,
+        currentHeading_fix_rad:float
     ) -> float:
     """
-    Calculate the steering angle based on the difference between the desired heading and the current heading, scaled by a proportional gain.
+    Calculate the steering angle based on the difference between the desired heading
+    and the current heading, scaled by a proportional gain.
 
     Parameters:
         k:
-            The proportional gain used to scale the heading error,
+            The proportional gain used to scale the heading error,  
             this is a constant.
-        desired_heading_rad:
+        desiredHeading_fix_rad:
             The desired heading the car should be facing,
             measured in radians.
-        current_heading_rad:
+        currentHeading_fix_rad:
             The current heading the car is facing,
             measured in radians.
 
     Returns:
-        steering_angle_rad:
+        steeringAngle_rad:
             The steering angle the car should use to correct its heading,
             measured in radians.
     """
-    return k*(desired_heading_rad - current_heading_rad)
+    return k*(desiredHeading_fix_rad - currentHeading_fix_rad)
