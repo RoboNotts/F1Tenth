6fb9dd8162ac59b79f8c5270953ce80edb0acc42
psyaf5 <psyaf5@exmail.nottingham.ac.uk>
2025-03-03 11:52:15 +0000

    Capitalised the f in frame for variable names as well as doxygen comments

--- F1TenthAlgorithms/dead_reckoning.py
+++ F1TenthAlgorithms/dead_reckoning.py
@@ -3,33 +3,33 @@ from numpy.typing import ArrayLike
 
 
 def dead_reckoning(
-        position_fix_m: ArrayLike[float],
-        velocity_fix_mps: ArrayLike[float],
+        position_Fix_m: ArrayLike[float],
+        velocity_Fix_mps: ArrayLike[float],
         changeInTime_s: ArrayLike[float],
 ) -> ArrayLike[float]:
     """
     Performs simple dead reckoning using Euler's method of integration.
 
     Parameters:
-        position_fix_m: Previous (x, y) location in the fixed coordinate frame (meters).
-        velocity_fix_mps: Velocity vector (v_x, v_y) in the fixed coordinate frame (meters per second).
+        position_Fix_m: Previous (x, y) location in the fixed coordinate frame (meters).
+        velocity_Fix_mps: Velocity vector (v_x, v_y) in the fixed coordinate frame (meters per second).
         changeInTime_s: Total time interval for integration (seconds).
 
     Returns:
-        updatedPosition_fix_m:
+        updatedPosition_Fix_m:
             Updated location in the fixed coordinate frame (meters).
     """
 
     # checking input position_fix_m (previous location)
-    position_fix_m = np.array(position_fix_m)
+    position_Fix_m = np.array(position_Fix_m)
 
     # checking velocity_fix_mps
-    velocity_fix_mps = np.array(velocity_fix_mps)
+    velocity_Fix_mps = np.array(velocity_Fix_mps)
 
     # checking changeInTime_s (change in time)
     changeInTime_s = np.array(changeInTime_s)
 
     # Apply Euler's method: new_position = prev_position + velocity * time
-    updatedPosition_fix_m = position_fix_m + changeInTime_s * velocity_fix_mps
+    updatedPosition_Fix_m = position_Fix_m + changeInTime_s * velocity_Fix_mps
 
-    return updatedPosition_fix_m
+    return updatedPosition_Fix_m
