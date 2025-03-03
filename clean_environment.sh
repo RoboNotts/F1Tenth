2e0b59aa16bf3a44d5df48ebff691b3242632d87
Shailin <shailinchavda@gmail.com>
2025-03-03 12:25:32 +0000

    [F1T-03_creating_ros2_package] Made bash scripts look pretty and made them executable

--- clean_environment.sh
+++ clean_environment.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-rm -r build/*
+
+# Clear everything in the build dir
+rm -r build
+mkdir build
+
+# Clear everything in the log dir
+rn -r log
+mkdir log
+
+# Clear everything in the install dir
+rn -r install
+mkdir install
