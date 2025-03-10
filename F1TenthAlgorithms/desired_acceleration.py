0576847498a93f6d5ae02e1e52856a4672625cf7
psyaf5 <psyaf5@exmail.nottingham.ac.uk>
2025-03-10 11:57:39 +0000

    Changed frame fix to Fix

--- F1TenthAlgorithms/desired_acceleration.py
+++ F1TenthAlgorithms/desired_acceleration.py
@@ -2,33 +2,33 @@ from numpy.typing import ArrayLike
 
 
 def find_desired_acceleration(
-    currentVelocity_fix_mps: ArrayLike[float],
-    desiredVelocity_fix_mps: ArrayLike[float],
+    currentVelocity_Fix_mps: ArrayLike[float],
+    desiredVelocity_Fix_mps: ArrayLike[float],
     tauTimeConstant_s: float = 1.0
 ) -> float:
     """
     Calculate the acceleration needed to reach the desired velocity.
 
     Parameters:
-        currentVelocity_fix_mps:
+        currentVelocity_Fix_mps:
             Current velocity of the car in its own frame of reference,
             measured in metres per second.
 
-        desiredVelocity_fix_mps:
+        desiredVelocity_Fix_mps:
             Desired velocity of the car in its own frame of reference,
             measured in metres per second.
 
         tauTimeConstant_s:
             The amount of seconds to reach the desired velocity
-            `desiredVelocity_fix_mps` (default 1.0).
+            `desiredVelocity_Fix_mps` (default 1.0).
 
     Returns:
         desired_acceleration:
             The acceleration in m/s^2 needed to reach velocity
-            `desiredVelocity_fix_mps` in `tauTimeConstant_s` seconds.
+            `desiredVelocity_Fix_mps` in `tauTimeConstant_s` seconds.
     """
 
     # Find the desiered change in velocity in velocity
-    velocityDiff_fix_mps = desiredVelocity_fix_mps - currentVelocity_fix_mps
+    velocityDiff_Fix_mps = desiredVelocity_Fix_mps - currentVelocity_Fix_mps
 
-    return velocityDiff_fix_mps / tauTimeConstant_s
+    return velocityDiff_Fix_mps / tauTimeConstant_s
